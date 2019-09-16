6558c7616e53ee481f77e52b4b58fb3d63765e99
Palopez <32514434+Palopez@users.noreply.github.com>
2019-09-16 10:31:21 -0300

    SQL to Excel Ver.1.0

---
+++ SQL2Excel.py
@@ -0,0 +1,17 @@
+﻿# Created by pablo.lopez
+#Global variables#
+import pyodbc
+import pandas as pd
+export_file = 'export.xlsx'
+#end of global variables#
+#--#
+#Connection to the database. Put credentials of the db. db name, user, password and driver type
+cnxn = pyodbc.connect("Driver={SQL Server};SERVER=server_name;Database=database_name;UID=database_user;PWD=database_password")
+
+script = """
+SELECT * FROM database;
+"""
+df = pd.read_sql_query(script, cnxn)
+writer = pd.ExcelWriter(export_file)
+df.to_excel(writer, sheet_name='Sheet1', index = False)
+writer.save()
